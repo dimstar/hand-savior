dbefb69cb65e50ca49db363e7913ba424b1c1cd1
dimstar <kylermagee@gmail.com>
2018-04-29 11:19:21 -0700

    proof of concept failed... :(

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const api_key = process.env.GKEY;
 const Speakable = require('speakable');
 const speakable = new Speakable({key: api_key});
+const robot = require('robotjs');
+
 const log = console.log;
 
 // log(api_key);
@@ -22,12 +24,24 @@ speakable.on('speechReady', ()=>{
 speakable.on('error', function(err) {
     console.log('onError:');
     console.log(err);
-    l
+    speakable.recordVoice();
 });
    
 speakable.on('speechResult', function(recognizedWords) {
-    console.log('onSpeechResult:')
-    console.log(recognizedWords);
+    log('onSpeechResult:')
+    log(recognizedWords);
+    if((recognizedWords.indexOf('swipe') > -1) && (recognizedWords.indexOf('left') > -1)){
+        robot.keyToggle('control', 'down');
+        robot.keyTap('left');
+        robot.keyToggle('control', 'up');
+    }
+
+    if((recognizedWords.indexOf('swipe') > -1) && (recognizedWords.indexOf('right') > -1)){
+        robot.keyToggle('control', 'down');
+        robot.keyTap('right');
+        robot.keyToggle('control', 'up');
+    }
+    
     speakable.recordVoice();
 });
    
